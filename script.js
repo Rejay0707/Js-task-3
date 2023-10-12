cac9f92972a1bff26d5b49150a8d8a2fbc748b67
Rejay0707 <139040231+Rejay0707@users.noreply.github.com>
2023-10-12 11:25:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+const clickMeButton = document.getElementById('click-Me');
+
+const boxcon = document.getElementById('boxcon');
+
+function changebg() {
+  boxcon.style.backgroundColor = 'red';
+}
+function changeco() {
+  boxcon.style.backgroundColor = 'yellow';
+}
+function changecl() {
+  boxcon.style.backgroundColor = 'green';
+}
